e09f27c5df8c1f2c4c8104a22bf220d194c4b423
boB Rudis <bob@rud.is>
2019-07-30 20:01:13 -0400

    R package repo initialization complete

---
+++ R/emailrep-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/emailrep>
+#' - BugReports: <https://gitlab.com/hrbrmstr/emailrep/issues>
+#' 
+#' @md
+#' @name emailrep
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+"_PACKAGE"
